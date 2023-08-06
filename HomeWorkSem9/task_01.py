fbc507f52bebed9d9c3e843ff2c2d93a9061ef7d
Aleksandr <uorkansk@yandex.ru>
2023-08-06 19:01:49 +0700

    решение домашних задач семинар 9 задача 1

--- HomeWorkSem9/task_01.py
+++ HomeWorkSem9/task_01.py
@@ -5,47 +5,49 @@
 # ○ Декоратор, сохраняющий переданные параметры и результаты работы функции в json файл.
 # 📌 Соберите пакет с играми из тех файлов, что уже были созданы в рамках курса
 import csv
-import functools
+import json
 import random
 
 
-def quadratic_equation(line):
-    a, b, c = map(int, line)
-    if a == 0:
-        print(f'None: a = 0')
-    discriminant = pow(b, 2) - 4 * a * c
-    if discriminant < 0:
-        print(f'Not root')
-    elif discriminant == 0:
-        x = -b / (2 * a)
-        print(f'one root {x}')
-    else:
-        x1 = (-b + pow(discriminant, 0.5)) / (2 * a)
-        x2 = (-b - pow(discriminant, 0.5)) / (2 * a)
-        answer = x1, x2
-        print(f'two root {answer}')
+def quadratic_equation(file_name):
+    results = []
+
+    def read_data_scv():
+        with open(file_name, 'r', newline='') as f:
+            csv_file = csv.reader(f)
+            for line in csv_file:
+                a, b, c = line
+                a, b, c = int(a), int(b), int(c)
+                if a == 0:
+                    continue
+                discriminant = pow(b, 2) - 4 * a * c
+                if discriminant < 0:
+                    answer = 'Not root'
+                elif discriminant == 0:
+                    x = -b / (2 * a)
+                    answer = x
+                else:
+                    x1 = (-b + pow(discriminant, 0.5)) / (2 * a)
+                    x2 = (-b - pow(discriminant, 0.5)) / (2 * a)
+                    answer = x1, x2
+
+                results.append({"a, b, c": line, "root": answer})
+                with open("output.json", "w") as json_file:
+                    json.dump(results, json_file, indent=2)
+
+    return read_data_scv()
 
 
 def generate_csv_file(file_name):
     count_num = 3
-    # count_str = random.randint(100, 1000)
-    count_str = 5
+    count_str = random.randint(100, 1000)
     with open(file_name, "w", newline="") as file:
         for _ in range(count_str):
             writer = csv.writer(file)
             writer.writerow([random.randint(-10, 10) for _ in range(count_num)])
 
 
-def read_data_scv(file_name):
-    with open(file_name, 'r', newline='') as f:
-        csv_file = csv.reader(f)
-        for line in csv_file:
-            return line
-
-
-
-
-
-name_file = "number.csv"
-generate_csv_file(name_file)
-print(quadratic_equation(read_data_scv(name_file)))
+if __name__ == '__main__':
+    name_file = 'number.csv'
+    generate_csv_file(name_file)
+    quadratic_equation(name_file)
